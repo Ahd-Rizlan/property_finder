0207c2283d78ff7c3960391ef9e4340b154eb9b5
Ahamed Rizlan <arizlan@mitrai.com>
2024-12-25 21:56:13 +0530

    used navbar on bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,57 @@
 import { Link } from "react-router-dom";
 import React from "react";
+
 function NavBar() {
   return (
-    <nav className="NavBar">
-      <Link to="/" className="SiteTitle">
-        <img src="propertyFinder_Nav_logo.jpg" alt="Site Logo" />
-      </Link>
-      <ul>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/Favourites">My Favourites</Link>
-        </li>
-        <li>
-          <Link to="/ContactUs">Contact Us</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        {/* Brand Logo */}
+        <Link to="/" className="navbar-brand d-flex align-items-center">
+          <img
+            src="propertyFinder_Nav_logo.jpg"
+            alt="Site Logo"
+            className="rounded-circle me-2"
+            style={{ height: "40px", width: "40px" }}
+          />
+          Property Finder
+        </Link>
+
+        {/* Hamburger Menu */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Collapsible Links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Favourites" className="nav-link">
+                My Favourites
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/ContactUs" className="nav-link">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
+
 export default NavBar;
